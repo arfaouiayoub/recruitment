5ca88bb625da57105ebe23cf14740248f4b98a88
Arfaoui <ayoub.arfaoui-ext@faurecia.com>
2020-11-16 16:08:31 +0100

    [DEV] Add timeline feature

--- engineering/multimedia/videoplayer-gtk3/videoplayer.c
+++ engineering/multimedia/videoplayer-gtk3/videoplayer.c
@@ -8,16 +8,18 @@
 #include <gdk/gdkx.h>
 
 #define TIME_STRING_LENGTH 13
+#define THUMBNAILS_NUMBER  10
 
 /* Structure to contain all our information, so we can pass it around */
 typedef struct _CustomData
 {
-  GstElement *playbin;    /* Our one and only pipeline */
-  GtkWidget *main_window; /* The uppermost window, containing all other windows */
-  GstState state;         /* Current state of the pipeline */
-  gint64 duration;        /* Duration of the clip, in nanoseconds */
-  gint64 position;        /* Position of the clip, in nanoseconds */
-  gint timer_id;          /* The ID of the timer source */
+  GstElement *playbin;     /* Our one and only pipeline */
+  GtkWidget *main_window;  /* The uppermost window, containing all other windows */
+  GstState state;          /* Current state of the pipeline */
+  gint64 duration;         /* Duration of the clip, in nanoseconds */
+  gint64 position;         /* Position of the clip, in nanoseconds */
+  gint timer_id;           /* The ID of the timer source */
+  GstElement *timelinebin; /* Timeline pipline to make thumbnails */
 } CustomData;
 
 /* Enumerates widget types */
@@ -26,14 +28,16 @@ enum widget_type
   WIDGET_TYPE_DURATION, /* Duration label widget */
   WIDGET_TYPE_POSITION, /* Position label widget */
   WIDGET_TYPE_SCALE,    /* Scale widget */
+  WIDGET_TYPE_TIMELINE, /* Timeline widget */
 
   WIDGET_TYPE_COUNT
 };
 
 static const gchar *widget_type_strings[] = {
-    [WIDGET_TYPE_DURATION] = "duration",
-    [WIDGET_TYPE_POSITION] = "position",
-    [WIDGET_TYPE_SCALE]    = "scale"
+    [WIDGET_TYPE_DURATION]    = "duration",
+    [WIDGET_TYPE_POSITION]    = "position",
+    [WIDGET_TYPE_SCALE]       = "scale",
+    [WIDGET_TYPE_TIMELINE]    = "timeline"
   };
 
 /* This function converts widget type to string */
@@ -60,7 +64,7 @@ static gchar *time_to_string(gint64 time)
   return res;
 }
 
-/* This function makes label text for a specific widget type
+/* This function makes label text from widget type
  * The returned string should be freed with g_free() when no longer needed.
 */
 static gchar *make_label_txt(enum widget_type type, gchar *duration)
@@ -79,23 +83,39 @@ static gchar *make_label_txt(enum widget_type type, gchar *duration)
   }
 }
 
-/* This function sets a given widget */
+/* This function sets a label text widget */
 static void set_label_txt(GtkWidget *label, enum widget_type type, CustomData *data) {
   g_return_if_fail(label != NULL);
   g_return_if_fail(data != NULL);
   g_return_if_fail(type < WIDGET_TYPE_SCALE);
 
   gint64 time;
-  if (type == WIDGET_TYPE_DURATION)
-    time =  data->duration;
-  else
-    time =  data->position;
+  switch (type) {
+    case WIDGET_TYPE_DURATION:
+      time =  data->duration;
+      break;
+    case WIDGET_TYPE_POSITION:
+      time =  data->position;
+      break;
+    default:
+      g_error("Cannot set label text: unexpected widget type");
+      return;
+  }
 
   gchar *time_str = time_to_string(time);
   gchar *label_txt = make_label_txt(type, time_str);
   gtk_label_set_text(GTK_LABEL(label), label_txt);
   g_free(time_str);
   g_free(label_txt);
+}
+
+/* This functions adds an image created from file to a widget */
+static void widget_add_image(GtkWidget *widget) {
+  g_return_if_fail(widget != NULL);
+
+  GtkWidget *image = gtk_image_new_from_file ("snapshot.png");
+  gtk_box_pack_start(GTK_BOX(widget), image, FALSE, FALSE, 2);
+  gtk_widget_show_all(widget);
 }
 
 /* Function to update a specific widget */
@@ -113,9 +133,21 @@ static void update_widget(CustomData *data, enum widget_type type)
   for (const GList *iter = children; iter != NULL; iter = g_list_next(iter))
   {
     GtkWidget *box = (GtkWidget *)(iter->data);
-    if (g_strcmp0(gtk_widget_get_name(box), "controls") != 0)
+    const gchar *box_name = gtk_widget_get_name(box);
+
+    /* Nothing to do for main hbbx widget */
+    if (g_strcmp0(box_name, "main_hbox") == 0)
       continue;
 
+    /* Process timeline widget */
+    if (g_strcmp0(box_name, "timeline") == 0) {
+      if (type != WIDGET_TYPE_TIMELINE)
+        continue;
+      widget_add_image(box);
+      break;
+    }
+
+    /* Process controls widgets */
     GList *control_children = gtk_container_get_children(GTK_CONTAINER(box));
     for (const GList *it = control_children; it != NULL; it = g_list_next(it))
     {
@@ -130,10 +162,107 @@ static void update_widget(CustomData *data, enum widget_type type)
       }
     }
     g_list_free(control_children);
-    break;
   }
 
   g_list_free(children);
+}
+
+/*This function extracts thumbnails using timeline pipeline */
+static void extract_thumbnails(CustomData *data, gint step) {
+  g_return_if_fail(data != NULL);
+
+  gint64 position;
+  GstElement *sink = NULL;
+  GstSample *sample;
+  gint width, height;
+  GdkPixbuf *pixbuf;
+  GError *error = NULL;
+  gboolean res;
+  GstMapInfo map;
+  GstStateChangeReturn ret;
+
+  /* set to PAUSED to make the first frame arrive in the sink */
+  ret = gst_element_set_state (data->timelinebin, GST_STATE_PAUSED);
+  switch (ret) {
+    case GST_STATE_CHANGE_FAILURE:
+      g_print ("failed to play the file\n");
+      return;
+    case GST_STATE_CHANGE_NO_PREROLL:
+      /* for live sources, we need to set the pipeline to PLAYING before we can
+       * receive a buffer. We don't do that yet */
+      g_print ("live sources not supported yet\n");
+      return;
+    default:
+      break;
+  }
+
+  gst_element_query_duration(data->timelinebin, GST_FORMAT_TIME, &data->duration);
+
+  position = (step+1) * data->duration * 10 / 100;
+
+  gst_element_seek_simple (data->timelinebin, GST_FORMAT_TIME,
+      GST_SEEK_FLAG_KEY_UNIT | GST_SEEK_FLAG_FLUSH, position);
+
+  g_object_get(data->timelinebin, "video-sink", &sink, NULL);
+
+  g_signal_emit_by_name (sink, "pull-preroll", &sample, NULL);
+  gst_object_unref (sink);
+
+if (sample) {
+    GstBuffer *buffer;
+    GstCaps *caps;
+    GstStructure *s;
+
+    /* get the snapshot buffer format now. We set the caps on the appsink so
+     * that it can only be an rgb buffer. The only thing we have not specified
+     * on the caps is the height, which is dependant on the pixel-aspect-ratio
+     * of the source material */
+    caps = gst_sample_get_caps (sample);
+    if (!caps) {
+      g_print ("could not get snapshot format\n");
+      return;
+    }
+    s = gst_caps_get_structure (caps, 0);
+
+    /* we need to get the final caps on the buffer to get the size */
+    res = gst_structure_get_int (s, "width", &width);
+    res |= gst_structure_get_int (s, "height", &height);
+    if (!res) {
+      g_print ("could not get snapshot dimension\n");
+      return;
+    }
+
+    /* create pixmap from buffer and save, gstreamer video buffers have a stride
+     * that is rounded up to the nearest multiple of 4 */
+    buffer = gst_sample_get_buffer (sample);
+    gst_buffer_map (buffer, &map, GST_MAP_READ);
+    pixbuf = gdk_pixbuf_new_from_data (map.data,
+        GDK_COLORSPACE_RGB, FALSE, 8, width, height,
+        GST_ROUND_UP_4 (width * 3), NULL, NULL);
+
+    /* save the pixbuf */
+    gdk_pixbuf_save (pixbuf, "snapshot.png", "png", &error, NULL);
+    gst_buffer_unmap (buffer, &map);
+  } else {
+    g_print ("could not make snapshot\n");
+  }
+}
+
+static gboolean timeline_make_thumbnails(CustomData *data) {
+  g_return_val_if_fail(data != NULL, FALSE);
+
+  static int count;
+  if (count< THUMBNAILS_NUMBER) {
+    extract_thumbnails(data, count);
+    update_widget(data, WIDGET_TYPE_TIMELINE);
+    count++;
+    return TRUE;
+  }
+
+  /* Free resources */
+  gst_element_set_state(data->timelinebin, GST_STATE_NULL);
+  gst_object_unref(data->timelinebin);
+  return G_SOURCE_REMOVE;
 }
 
 /* This function is called when the GUI toolkit creates the physical window that will hold the video.
@@ -190,7 +319,10 @@ static void open_cb(GtkButton *button, CustomData *data)
     char *filename;
     GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
     filename = gtk_file_chooser_get_uri(chooser);
-    /* Set the URI to play */
+    /* Set the URI to timelinebin */
+    g_object_set(data->timelinebin, "uri", filename, NULL);
+    g_timeout_add(1000, (GSourceFunc) timeline_make_thumbnails, data);
+    /* Set the URI to playbin */
     g_object_set(data->playbin, "uri", filename, NULL);
     gst_element_set_state(data->playbin, GST_STATE_PLAYING);
     g_free(filename);
@@ -230,6 +362,7 @@ static void create_ui(CustomData *data)
   GtkWidget *duration;                                               /* Duration label */
   GtkWidget *position;                                               /* Position label */
   GtkWidget *scale;                                                  /* Scale widget */
+  GtkWidget *timeline;                                               /* Timeline */
 
   data->main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   g_signal_connect(G_OBJECT(data->main_window), "delete-event", G_CALLBACK(delete_event_cb), data);
@@ -254,7 +387,7 @@ static void create_ui(CustomData *data)
   set_label_txt(position, WIDGET_TYPE_POSITION, data);
 
   scale = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 1, 0.01);
-  g_object_set(scale, "width-request", 400, NULL);
+  g_object_set(scale, "width-request", 1350, NULL);
   gtk_widget_set_name(scale, "scale");
   g_signal_connect(G_OBJECT(scale), "change-value", G_CALLBACK(scale_cb), data);
 
@@ -276,11 +409,15 @@ static void create_ui(CustomData *data)
   gtk_widget_set_name(main_hbox, "main_hbox");
   gtk_box_pack_start(GTK_BOX(main_hbox), video_window, TRUE, TRUE, 0);
 
+  timeline =  gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+  gtk_widget_set_name(timeline, "timeline");
+
   main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
   gtk_box_pack_start(GTK_BOX(main_box), main_hbox, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(main_box), controls, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(main_box), timeline, FALSE, FALSE, 0);
   gtk_container_add(GTK_CONTAINER(data->main_window), main_box);
-  gtk_window_set_default_size(GTK_WINDOW(data->main_window), 640, 480);
+  gtk_window_set_default_size(GTK_WINDOW(data->main_window), 1600, 680);
 
   gtk_widget_show_all(data->main_window);
 }
@@ -350,9 +487,10 @@ static void state_changed_cb(GstBus *bus, GstMessage *msg, CustomData *data)
     else if (new_state == GST_STATE_PAUSED)
     {
       /* Remove timer to avoid updating current position */
-      if (data->timer_id > 0)
+      if (data->timer_id > 0) {
         g_source_remove(data->timer_id);
         data->timer_id = -1;
+      }
     }
   }
 }
@@ -363,6 +501,7 @@ int main(int argc, char *argv[])
   GstStateChangeReturn ret;
   GstBus *bus;
   GstElement *video_sink;
+  GstElement *app_sink;
 
   /* Initialize GTK */
   gtk_init(&argc, &argv);
@@ -383,7 +522,21 @@ int main(int argc, char *argv[])
 
   if (!data.playbin)
   {
-    g_printerr("Not all elements could be created.\n");
+    g_printerr("Not all playbin elements could be created.\n");
+    return -1;
+  }
+
+  data.timelinebin = gst_element_factory_make("playbin", "timelinebin");
+  app_sink = gst_element_factory_make("appsink", "videosink");
+  GstCaps *caps  = gst_caps_from_string ("video/x-raw,format=RGB,width=160,pixel-aspect-ratio=1/1");
+       
+  g_object_set(app_sink, "caps", caps, NULL);
+  gst_caps_unref(caps);
+  g_object_set(data.timelinebin, "video-sink", app_sink, NULL);
+
+  if (!data.timelinebin)
+  {
+    g_printerr("Not all timelinebin elements could be created.\n");
     return -1;
   }
 
